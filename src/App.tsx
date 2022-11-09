9e657ef1d2369b9caf4cd978445f70476988e8e1
ryogo-ito <ryogoito0907@gmail.com>
2022-11-10 03:21:48 +0900

    todolistコンポーネントの作成実施

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { TextField } from './ui/components/elements/TextField';
+import { TodoList } from './ui/components/modules/TodoList';
+import { MOCK_TODO_LIST } from './mock/todolist';
 
 export const App = () => {
   return (
     <div>
       <TextField />
+      <TodoList todoList={MOCK_TODO_LIST} header={'TODO'} />
     </div>
   );
 };

976b9b43ad1933463ad5a69f9153e873f8fe1421
ryogo-ito <ryogoito0907@gmail.com>
2022-11-13 18:21:15 +0900

    mockの修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import 'antd/dist/antd.css';
-import { MOCK_TODO_LIST } from './mock/todolist';
+import { MOCK_LIST } from './mock/list';
 import { EditList } from './ui/components/modules/EditList';
 
 export const App = () => {
   return (
     <div>
       <EditList
-        list={MOCK_TODO_LIST}
+        list={MOCK_LIST}
         headerText={'todo'}
         onEditButtonClick={() => alert('編集')}
         onDeleteButtonClick={() => alert('削除')}
